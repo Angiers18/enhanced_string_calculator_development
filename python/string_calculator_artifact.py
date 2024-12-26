0f65f196f7f9cd24c5f40778d42d634823327fa9
Angie <angiesamper18@gmail.com>
2024-12-26 16:20:45 -0300

    Added support for custom delimiters #4

--- python/string_calculator_artifact.py
+++ python/string_calculator_artifact.py
@@ -5,7 +5,8 @@ def add(input_str):
         return 0
 
     try:
-        str_replace = input_str.replace("\n", ",")
+        str_replace = ",".join(elem for elem in input_str if elem.isdigit())
+        # str_replace = input_str.replace("\n", ",")
         str_split = str_replace.split(",")
         
         return sum([int(i) for i in str_split])
--- python/test_string_calculator_artifact.py
+++ python/test_string_calculator_artifact.py
@@ -13,7 +13,9 @@ class Test_String_Calculator(unittest.TestCase):
         self.assertEqual(add("1,2,3,4,5,6"), 21)
         self.assertEqual(add("1,2\n3,4\n5,6\n7"), 28)
         self.assertEqual(add("1\n2,3\n4,5,6\n7,8"), 36)
-        self.assertEqual(add("2,a3"), "Invalid Value")
+        self.assertEqual(add("2,a3"), 5)
+        self.assertEqual(add("//;\n1;2"), 3)
+        self.assertEqual(add("/1,2/;\n3ESTO?ES.UN-TEST'5,2,2"), 15)
 
 
 if __name__ == '__main__':
