84cbf24dcb694ecc74b6c3ccb219e8672a675d4b
Angie <angiesamper18@gmail.com>
2025-01-12 22:25:23 -0300

    Implemented Add method for 0-2 numbers. #11

---
+++ f#/StringCalculator/Program.fs
@@ -0,0 +1,23 @@
+﻿module Program
+
+open System
+
+
+[<EntryPoint>]
+let main argv =
+    let add (inputStr: string) : int =
+        if inputStr = "" then 0
+        else
+            let splitStr = inputStr.Split(',') |> Array.map int
+            if splitStr |> Array.exists (fun x -> x > 2) then
+                failwith "No se puede sumar números mayores a 2"
+            else
+                splitStr |> Array.sum
+
+    0 // Código de salida
+
+
+
+
+
+
